94056798db9845263fbe23f0cfee3efa8581880d
BruGeth <g15bru03@hotmail.com>
2025-10-01 12:29:39 -0500

    feat: refactorizar App.js para incluir configuración de navegación y pantallas

--- App.js
+++ App.js
@@ -1,5 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import UsersScreen from './screens/UsersScreen';
+import PostsScreen from './screens/PostsScreen';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
