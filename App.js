12cab0402d5f9c1f3f4d6d8c9bef94db0d873a7e
BruGeth <g15bru03@hotmail.com>
2025-10-01 12:29:55 -0500

    feat: actualizar App.js para implementar navegación entre pantallas

--- App.js
+++ App.js
@@ -8,18 +8,11 @@ const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Usuarios" component={UsersScreen} />
+        <Stack.Screen name="Posts" component={PostsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
